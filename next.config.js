fc051476b1a6c7123f17e0bc1c6a7d12841a6e17
YouMingYeh <ym911216@gmail.com>
2024-09-17 23:28:44 +0800

    feat: Add index, about pages

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const isProd = process.env.NODE_ENV === "production";
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  latex: true,
+});
+
+const nextConfig = {
+  basePath: isProd ? "/cs-book" : "",
+  assetPrefix: isProd ? "https://youmingyeh.github.io/cs-book" : undefined,
+  reactStrictMode: true,
+  output: isProd ? "export" : "standalone",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
+module.exports = withNextra(nextConfig);
---
+++ theme.config.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: (
+    <span
+      style={{
+        fontWeight: "bold",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.5em",
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-book-open-text"
+      >
+        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
+        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
+        <path d="M6 8h2" />
+        <path d="M6 12h2" />
+        <path d="M16 8h2" />
+        <path d="M16 12h2" />
+      </svg>
+      Adastra
+    </span>
+  ),
+  project: {
+    link: "https://github.com/adastra-tw/docs",
+  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: "https://github.com/adastra-tw/docs",
+  footer: {
+    text: "Created by Adastra Team",
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Adastra" />
+      <meta
+        property="og:description"
+        content="Adastra documentation site"
+      />
+    </>
+  ),
+  useNextSeoProps: () => ({
+    titleTemplate: '%s – Adastra',
+    openGraph: {
+      type: "website",
+      url: "https://github.com/adastra-tw/docs",
+      title: "Adastra",
+      description: "Adastra documentation site",
+    },
+  }),
+  sidebar: {
+     defaultMenuCollapseLevel: 1
+  },
+  
+};
+
+export default config;
